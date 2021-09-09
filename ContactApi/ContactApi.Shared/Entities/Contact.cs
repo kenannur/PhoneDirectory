7123b73c5002b3f13d299178a38c17dc7d7f28a3
Kenan Nur <kenannur@hotmail.com>
2021-09-09 18:17:10 +0300

    Update Contact.cs

--- ContactApi/ContactApi.Shared/Entities/Contact.cs
+++ ContactApi/ContactApi.Shared/Entities/Contact.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace ContactApi.Shared.Entities
 {
@@ -10,6 +11,7 @@ namespace ContactApi.Shared.Entities
 
         public string Firm { get; set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public virtual List<ContactInformation> Informations { get; set; }
     }
 }
