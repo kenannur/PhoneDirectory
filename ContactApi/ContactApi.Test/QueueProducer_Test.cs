98886ced01c400b986cc60424c0efa597644c4bb
Kenan Nur <kenannur@hotmail.com>
2021-09-10 16:32:43 +0300

    Create QueueProducer_Test.cs

---
+++ ContactApi/ContactApi.Test/QueueProducer_Test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using ContactApi.Messaging.Producer.Client;
+using ContactApi.Messaging.Producer.Settings;
+using Moq;
+using Xunit;
+
+namespace ContactApi.Test
+{
+    public class QueueProducer_Test
+    {
+        public QueueProducer_Test()
+        { }
+
+        [Theory]
+        [MemberData(nameof(GetRabbitMqSettingsDatas))]
+        public void QueueProducer_SendReportRequest(IRabbitMqSettings settings, bool expected)
+        {
+            var queueProducer = new QueueProducer(settings);
+            var result = queueProducer.SendReportRequest(It.IsAny<string>());
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> GetRabbitMqSettingsDatas() => new List<object[]>
+        {
+            new object[] { GetRabbitMqSettings(), true },
+            new object[] { new RabbitMqSettings(), false }
+        };
+
+        private static IRabbitMqSettings GetRabbitMqSettings() => new RabbitMqSettings
+        {
+            Hostname = "localhost",
+            Username = "guest",
+            Password = "guest",
+            QueueName = "ReportQueue"
+        };
+    }
+}
