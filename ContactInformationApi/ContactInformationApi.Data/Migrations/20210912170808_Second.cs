3ef15991919707925671de7a5051a8da48d89d41
Kenan Nur <kenannur@hotmail.com>
2021-09-12 20:10:50 +0300

    ContactInformationApi initial migration

---
+++ ContactInformationApi/ContactInformationApi.Data/Migrations/20210912170808_Second.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ContactInformationApi.Shared.Entities;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactInformationApi.Data.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:contact_type", "unknown,phone_number,email_address,location");
+
+            migrationBuilder.CreateTable(
+                name: "ContactInformations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ContactId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<ContactType>(type: "contact_type", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactInformations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactInformations");
+        }
+    }
+}
