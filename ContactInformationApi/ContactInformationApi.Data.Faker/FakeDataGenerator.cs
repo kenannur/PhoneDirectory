2f27e060f355796570e76818b4cf852ee1ece66d
Kenan Nur <kenannur@hotmail.com>
2021-09-12 19:37:13 +0300

    ContactInformationApi FakeDataGenerator created.

---
+++ ContactInformationApi/ContactInformationApi.Data.Faker/FakeDataGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bogus;
+using ContactInformationApi.Shared.Entities;
+
+namespace ContactInformationApi.Data.Faker
+{
+    public static class FakeDataGenerator
+    {
+        private const string locale = "tr";
+
+        public static List<ContactInformation> Prepare(Guid contactId, int count = 3)
+        {
+            Randomizer.Seed = new Random(8675309);
+
+            var contactInformationFaker = new Faker<ContactInformation>(locale)
+                .RuleFor(x => x.Id, Guid.NewGuid)
+                .RuleFor(x => x.ContactId, contactId)
+                .RuleFor(x => x.Type, x => x.PickRandom<ContactType>())
+                .RuleFor(x => x.Value, (f, c) =>
+                {
+                    if (c.Type == ContactType.PhoneNumber)
+                    {
+                        return f.Person.Phone;
+                    }
+                    else if (c.Type == ContactType.EmailAddress)
+                    {
+                        return f.Person.Email;
+                    }
+                    else if (c.Type == ContactType.Location)
+                    {
+                        return f.Person.Address.City;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                });
+
+            return contactInformationFaker
+                .Generate(count)
+                .Where(x => x.Type != ContactType.Unknown)
+                .ToList();
+        }
+    }
+}
